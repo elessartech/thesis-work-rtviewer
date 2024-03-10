909d09cee42603987cf3642eb5967faaa7c7ef9f
Maksim Ilmast <maksim.ilmast@mvision.ai>
2024-03-10 17:35:27 +0200

    segmentation brush added

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,6 +4,7 @@ import {
     RenderingEngine,
     setVolumesForViewports,
     volumeLoader,
+    CONSTANTS
 } from '@cornerstonejs/core'
 import { ViewportType } from '@cornerstonejs/core/dist/esm/enums'
 import { IRenderingEngine } from '@cornerstonejs/core/dist/esm/types'
@@ -17,39 +18,44 @@ import {
     toolGroupId,
     segmentationId,
     stackImageSyncronizerId,
+    viewportId3,
 } from '../constants'
-import {
-    AngleTool,
-    BidirectionalTool,
-    BrushTool,
-    CircleROITool,
-    EllipticalROITool,
-    LengthTool,
-    PanTool,
-    ProbeTool,
-    RectangleROITool,
-    SegmentationDisplayTool,
-    StackScrollMouseWheelTool,
-    SynchronizerManager,
-    ToolGroupManager,
-    WindowLevelTool,
-    ZoomTool,
-    addTool,
-    segmentation,
-} from '@cornerstonejs/tools'
+import * as cornerstoneTools from '@cornerstonejs/tools';
 import {
     MouseBindings,
     SegmentationRepresentations,
 } from '@cornerstonejs/tools/dist/esm/enums'
 import { createStackImageSynchronizer } from '@cornerstonejs/tools/dist/esm/synchronizers'
+import setCtTransferFunctionForVolumeActor from '../helpers/metadata/setCtTransferFunctionForVolumeActor'
+import addManipulationBindings from '../helpers/metadata/addManipulationBindings'
+
+const {
+    addTool,
+    SegmentationDisplayTool,
+    ToolGroupManager,
+    Enums: csToolsEnums,
+    segmentation,
+    BrushTool,
+    PanTool,
+    ZoomTool,
+    StackScrollMouseWheelTool,
+    WindowLevelTool,
+    BidirectionalTool,
+    RectangleROITool,
+    EllipticalROITool,
+    LengthTool,
+    ProbeTool,
+    AngleTool,
+    CircleROITool,
+    SynchronizerManager,
+    PlanarFreehandContourSegmentationTool,
+    SegmentSelectTool
+  } = cornerstoneTools;
 
 const Main = () => {
     const sagitalCanvasWrapRef = useRef<HTMLDivElement | null>(null)
     const coronalCanvasWrapRef = useRef<HTMLDivElement | null>(null)
     const axialCanvasWrapRef = useRef<HTMLDivElement | null>(null)
-    const sagitalPetCanvasWrapRef = useRef<HTMLDivElement | null>(null)
-    const coronalPetCanvasWrapRef = useRef<HTMLDivElement | null>(null)
-    const axialPetCanvasWrapRef = useRef<HTMLDivElement | null>(null)
     const [ctImageIds, setCtImageIds] = useState<string[]>([])
     const [renderingEngine, setRenderingEngine] =
         useState<IRenderingEngine | null>(null)
@@ -70,16 +76,13 @@ const Main = () => {
         if (renderingEngine) {
             ;(async () => await setImage())()
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [renderingEngine])
 
     useEffect(() => {
         if (activeTool) {
             const toolGroup = ToolGroupManager.getToolGroup(toolGroupId);
             const currActivePrimaryBtnTool = toolGroup.getActivePrimaryMouseButtonTool();
-            console.log(currActivePrimaryBtnTool)
             if (currActivePrimaryBtnTool) toolGroup.setToolDisabled(currActivePrimaryBtnTool);
-            console.log(activeTool)
             toolGroup.setToolActive(activeTool, {
                 bindings: [{ mouseButton: MouseBindings.Primary }],
             });
@@ -88,54 +91,34 @@ const Main = () => {
 
     const addTools = () => {
         ToolGroupManager.createToolGroup(toolGroupId)
-        addTool(SegmentationDisplayTool)
-        addTool(BrushTool)
-        addTool(StackScrollMouseWheelTool)
-        addTool(WindowLevelTool)
-        addTool(BidirectionalTool)
-        addTool(RectangleROITool)
-        addTool(EllipticalROITool)
-        addTool(PanTool)
-        addTool(LengthTool)
-        addTool(ProbeTool)
-        addTool(ZoomTool)
-        addTool(AngleTool)
-        addTool(CircleROITool)
+        addTool(PlanarFreehandContourSegmentationTool)
+        addTool(SegmentationDisplayTool);
+        addTool(SegmentSelectTool);
     }
 
     const setImage = async () => {
         const toolGroup = ToolGroupManager.getToolGroup(toolGroupId)
-        toolGroup.addTool(StackScrollMouseWheelTool.toolName, { loop: true })
-        toolGroup.addTool(WindowLevelTool.toolName)
-        toolGroup.addTool(PanTool.toolName)
-        toolGroup.addTool(ZoomTool.toolName)
-        toolGroup.addTool(AngleTool.toolName)
-        toolGroup.addTool(LengthTool.toolName)
-        toolGroup.addTool(CircleROITool.toolName)
-        toolGroup.addTool(RectangleROITool.toolName)
-        toolGroup.setToolEnabled(StackScrollMouseWheelTool.toolName)
-        toolGroup.setToolActive(StackScrollMouseWheelTool.toolName)
-        toolGroup.setToolActive(WindowLevelTool.toolName, {
+        toolGroup.addTool(PlanarFreehandContourSegmentationTool.toolName);
+        addManipulationBindings(toolGroup);
+
+
+        toolGroup.addTool(SegmentationDisplayTool.toolName);
+        toolGroup.addTool(SegmentSelectTool.toolName);
+
+
+        toolGroup.setToolEnabled(SegmentationDisplayTool.toolName);
+        toolGroup.setToolActive(SegmentSelectTool.toolName);
+
+
+        toolGroup.setToolActive(PlanarFreehandContourSegmentationTool.toolName, {
             bindings: [
-                {
-                    mouseButton: MouseBindings.Primary, // Left Click
-                },
+              {
+                mouseButton: MouseBindings.Primary,
+              },
             ],
-        })
-        toolGroup.setToolActive(PanTool.toolName, {
-            bindings: [
-                {
-                    mouseButton: MouseBindings.Auxiliary, // Middle Click
-                },
-            ],
-        })
-        toolGroup.setToolActive(ZoomTool.toolName, {
-            bindings: [
-                {
-                    mouseButton: MouseBindings.Secondary, // Right Click
-                },
-            ],
-        })
+          });
+
+
         createStackImageSynchronizer(stackImageSyncronizerId)
         const axialViewportElement = axialCanvasWrapRef.current
         const sagitalViewportElement = sagitalCanvasWrapRef.current
@@ -171,43 +154,56 @@ const Main = () => {
         })
         await volumeLoader.createAndCacheDerivedVolume(volumeId, {
             volumeId: segmentationId,
-        })
+          });
         const synchronizer = SynchronizerManager.getSynchronizer(
             stackImageSyncronizerId
         )
         if (!synchronizer) {
             return
         }
-        segmentation.addSegmentations([
+        await segmentation.addSegmentations([
             {
-                segmentationId,
-                representation: {
-                    type: SegmentationRepresentations.Labelmap,
-                    data: {
-                        volumeId: segmentationId,
-                    },
-                },
+              segmentationId,
+              representation: {
+                type: csToolsEnums.SegmentationRepresentations.Contour,
+              },
             },
-        ])
+          ]);
+
         renderingEngine.setViewports(viewportInput)
         toolGroup.addViewport(axialViewportId, renderingEngineId)
         toolGroup.addViewport(sagitalViewportId, renderingEngineId)
         toolGroup.addViewport(coronalViewportId, renderingEngineId)
-        synchronizer.add({ renderingEngineId, viewportId: axialViewportId })
-        synchronizer.add({ renderingEngineId, viewportId: sagitalViewportId })
-        synchronizer.add({ renderingEngineId, viewportId: coronalViewportId })
+        //synchronizer.add({ renderingEngineId, viewportId: axialViewportId })
+        //synchronizer.add({ renderingEngineId, viewportId: sagitalViewportId })
+        //synchronizer.add({ renderingEngineId, viewportId: coronalViewportId })
         volume.load()
+
         await setVolumesForViewports(
             renderingEngine,
-            [{ volumeId }],
+            [{ volumeId, callback: setCtTransferFunctionForVolumeActor }],
             [axialViewportId, sagitalViewportId, coronalViewportId]
-        )
-        await segmentation.addSegmentationRepresentations(toolGroupId, [
-            {
+          );
+          await segmentation.addSegmentationRepresentations(
+            toolGroupId,
+            [
+              {
                 segmentationId,
-                type: SegmentationRepresentations.Labelmap,
+                type: csToolsEnums.SegmentationRepresentations.Contour,
+              },
+            ]
+          );
+
+          segmentation.config.setToolGroupSpecificConfig(toolGroupId, {
+            renderInactiveSegmentations: true,
+            representations: {
+              CONTOUR: {
+                outlineWidthActive: 5,
+                outlineDashActive: '10, 10',
+              },
             },
-        ])
+          });
+        
         renderingEngine.renderViewports([
             axialViewportId,
             sagitalViewportId,
@@ -269,44 +265,6 @@ const Main = () => {
                     id="viewportElement"
                     className="viewportElement"
                     ref={coronalCanvasWrapRef}
-                    style={{
-                        width: '512px',
-                        height: '512px',
-                        margin: 'auto 1em',
-                    }}
-                    onContextMenu={(e) => e.preventDefault()}
-                ></div>
-            </div>
-            <div
-                className="container"
-                style={{ display: 'flex', flexDirection: 'row' }}
-            >
-                <div
-                    id="viewportElement"
-                    className="viewportElement"
-                    ref={axialPetCanvasWrapRef}
-                    style={{
-                        width: '512px',
-                        height: '512px',
-                        margin: 'auto 1em',
-                    }}
-                    onContextMenu={(e) => e.preventDefault()}
-                ></div>
-                <div
-                    id="viewportElement"
-                    className="viewportElement"
-                    ref={sagitalPetCanvasWrapRef}
-                    style={{
-                        width: '512px',
-                        height: '512px',
-                        margin: 'auto 1em',
-                    }}
-                    onContextMenu={(e) => e.preventDefault()}
-                ></div>
-                <div
-                    id="viewportElement"
-                    className="viewportElement"
-                    ref={coronalPetCanvasWrapRef}
                     style={{
                         width: '512px',
                         height: '512px',
