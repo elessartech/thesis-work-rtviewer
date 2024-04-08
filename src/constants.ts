0b8c7c1ea4f8a2b3f0f4155fe8fead2f45d91018
Maksim Ilmast <maksim.ilmast@mvision.ai>
2024-04-08 12:47:45 +0300

    removed redundant consts

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,5 @@
 export const renderingEngineId = 'myRenderingEngine'
-export const renderingEngineId2 = 'myRenderingEngine2'
 export const axialViewportId = 'CT_AXIAL_STACK'
-export const axialViewportId2 = 'CT_AXIAL_STACK_2'
 export const sagitalViewportId = 'CT_SAGITAL_STACK'
 export const coronalViewportId = 'CT_CORONAL_STACK'
 export const viewportId3 = 'CT_3D_STACK'
@@ -16,8 +14,6 @@ export const toolGroupId2 = 'CT_TOOLGROUP2'
 export const segmentationId = 'MY_SEGMENTATION_ID'
 export const cameraSynchronizerId = 'CAMERA_SYNCHRONIZER_ID'
 export const voiSynchronizerId = 'VOI_SYNCHRONIZER_ID'
-export const stackImageSyncronizerId = 'SYNC_ID'
-export const segmentationId2 = 'MY_SEGMENTATION_ID_2'
 export const synchronizerId = 'MY_SYNCHRONIZER_ID'
 export const viewportColors = {
     [axialViewportId]: 'rgb(200, 0, 0)',
